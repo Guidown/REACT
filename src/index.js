150b1252d87813dca3aede32d9c40aaf17c5769c
Guido <guidobombini@gmail.com>
2022-09-22 22:27:10 -0300

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import navbar from "./componentes/navbar.jsx";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root(<Navbar />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
